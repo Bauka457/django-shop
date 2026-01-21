4e05f33f3f2220cd5e5cfd188b2670a036e08144
Bauka457 <bauka457@example.com>
2026-01-21 10:43:26 +0500

    Fix settings.py for Render deploy

--- config/settings.py
+++ config/settings.py
@@ -25,11 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
-    DEBUG = os.getenv('DEBUG') == 'False'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+
 
 
 # Application definition
@@ -124,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
